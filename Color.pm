1e8f1b056e4007bff861e5e38802976c17157e74
Jeff Linahan <jeff.linahan@gmail.com>
2015-07-18 10:39:44 -0400

    colors customizable

--- Color.pm
+++ Color.pm
@@ -9,14 +9,15 @@ use warnings;
 use Exporter 'import';
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw($black $red $green $yellow $blue $magenta $cyan $ltgray $drkgray);
+our @EXPORT = qw($black $red $green $yellow $blue $magenta $cyan $ltgray $drkgray $white);
 
-our $black        = "\033[30m";
-our $red          = "\033[31m";
-our $green        = "\033[32m";
-our $yellow       = "\033[33m";
-our $blue         = "\033[34m";
-our $magenta      = "\033[35m";
-our $cyan         = "\033[36m";
-our $ltgray       = "\033[37m";
-our $drkgray      = "\033[1;30m";
+our $black	= "\033[30m";
+our $red	= "\033[31m";
+our $green	= "\033[32m";
+our $yellow	= "\033[33m";
+our $blue	= "\033[34m";
+our $magenta	= "\033[35m";
+our $cyan	= "\033[36m";
+our $ltgray	= "\033[37m";
+our $drkgray	= "\033[1;30m";
+our $white	= "\033[1;37m";
--- get.pl
+++ get.pl
@@ -3,30 +3,27 @@ use strict;
 use warnings;
 use Color;
 
-my $norm         = "\033[00m";
-my $background   = "\033[07m";
-my $brighten     = "\033[01m";
-my $underline    = "\033[04m";
-my $blink        = "\033[05m";
+my $norm	= "\033[00m";
+my $background	= "\033[07m";
+my $brighten	= "\033[01m";
+my $underline	= "\033[04m";
+my $blink	= "\033[05m";
 
 # Customize colors here...
-my $default      = $ltgray;
-my $gcc          = $magenta . $brighten;
-my $make         = $cyan;
-my $filename     = $yellow;
-my $linenum      = $cyan;
-my $trace        = $yellow;
-my $warning      = $green;
-my $comment      = $drkgray;
-my $tag_error    = "";
-my $error        = $tag_error . $yellow . $brighten;
-my $error_highlight = $brighten;
+my $default	= $norm;
+my $installed	= $blue;
+my $upgraded    = $blue;
+my $removed	= $red;
+my $error	= $red;
+my $obsolete	= $yellow;
+my $suggested	= $yellow;
+my $command	= $cyan;
+my $bytecol	= $green;
 
 # Get size of terminal
 shift @ARGV || 0;
 shift @ARGV || 0;
 
-my $in = 'unknown';
 $| = 1;
 my $skip = 0;
 
@@ -37,25 +34,25 @@ while( <STDIN> ){
 	my $orgline = $_;
 
 	s/  \+/ /g; # Remove multiple spaces
-	s/REMOVED:$/REMOVED:$red/;
-	s/installed:$/installed:$blue/;
-	s/^Removing ($pac)/Removing $red$1$default/;
-	s/^Unpacking ($pac)/Unpacking $blue$1$default/;
-	s/^Setting up ($pac)/Setting up $blue$1$default/;
-	s/($pac) is already/$blue$1$default is already/;
-	s/triggers for ($pac) /triggers for $blue$1 $default/;
-	s/Package '($pac)'/Package '$red$1$default'/;
-	s/unselected package ($pac)/unselected package $blue$1$default/;
+	s/REMOVED:$/REMOVED:$removed/;
+	s/installed:$/installed:$installed/;
+	s/^Removing ($pac)/Removing $removed$1$default/;
+	s/^Unpacking ($pac)/Unpacking $installed$1$default/;
+	s/^Setting up ($pac)/Setting up $installed$1$default/;
+	s/($pac) is already/$installed$1$default is already/;
+	s/triggers for ($pac) /triggers for $installed$1 $default/;
+	s/Package '($pac)'/Package '$error$1$default'/;
+	s/unselected package ($pac)/unselected package $installed$1$default/;
 	s/\/s\)$/\/s\)\r/;
 	s/not upgraded.$/not upgraded.\r/;
 	s/of archives.$/of archives.\r/;
 	s/will be used.$/will be used.\r/;
-	s/will be upgraded:/will be upgraded:$blue/;
-	s/^E:/$red\0E:/;
-	s/Suggested packages:/Suggested packages:$yellow$1/;
-	s/($bytes)/$green$1$default/g;
-	s/no longer required:/no longer required:$yellow/;
-	s/apt-get autoremove/$cyan$1apt-get autoremove$default/;
+	s/will be upgraded:/will be upgraded:$upgraded/;
+	s/^E:/$error\0E:/;
+	s/Suggested packages:/Suggested packages:$suggested$1/;
+	s/($bytes)/$bytecol$1$default/g;
+	s/no longer required:/no longer required:$obsolete/;
+	s/apt-get autoremove/$command$1apt-get autoremove$default/;
 
 	if( $_ !~ /^\s+/ ){print $norm, $default}
 	print $_;
